d2e2232b88a0ff1c4ae3089e5e78bf59200b6450
lestgabo <lestgabo@gmail.com>
2020-08-07 16:45:04 -0700

    Install mongoose, node, express, cors, dotenv - create server
    - Node server pointing to 8000
    - connected /register to mongodb

---
+++ src/controllers/RegisterController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+
+module.exports = {
+    async store(req, res) {
+        try {
+            const { firstName, lastName, password, email } = req.body;
+
+            const user = await User.create({
+                firstName: firstName,
+                lastName: lastName,
+                password: password,
+                email: email,
+            });
+
+            return res.json(user);
+        } catch (error) {
+            throw Error(`Error while registering a new user: ${error}`);
+        }
+    },
+};
