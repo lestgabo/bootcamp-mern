f5297d092c10b57966bd6137766412baf8ddc499
lestgabo <lestgabo@gmail.com>
2020-08-07 17:17:18 -0700

    Use bcrypt to hash password, use findOne for existing emails

---
+++ src/controllers/UserController.js
@@ -0,0 +1,28 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    async store(req, res) {
+        try {
+            const { firstName, lastName, password, email } = req.body;
+            const existentUser = await User.findOne({ email });
+
+            if (!existentUser) {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const user = await User.create({
+                    firstName,
+                    lastName,
+                    password: hashedPassword,
+                    email,
+                });
+                return res.json(user);
+            }
+
+            return res.status(400).json({
+                message: 'Email/user already exist! Do you want to login instead?',
+            });
+        } catch (error) {
+            throw Error(`Error while registering a new user: ${error}`);
+        }
+    },
+};
